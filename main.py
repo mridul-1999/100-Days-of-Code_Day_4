f1363366a941a05900439b26638afe11e627a394
Saif Mahmud <67528956+mridul-1999@users.noreply.github.com>
2023-04-05 16:11:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Day 4 Challenge
+print(" Welcome to yor Adventure simulator.")
+print(" I am going to ask you a bunch of questions and the create an epic story with you as the star!😎")
+print()
+
+name = input("Your Name : ")
+enemy = input("Your enemy's Name : ")
+super_Power = input("Your super Power : ")
+live = input("Where do you live : ")
+favorite_Food = input("What is your favorite Food : ")
+
+print()
+
+print("Hello",name,"! Your ability to", super_Power, "will make sure you never have to look at", enemy, "again. Go eat", favorite_Food, "as you walk down the streets of", live, "and use", super_Power, "for good and not evil!")
